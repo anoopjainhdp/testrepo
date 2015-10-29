fbb8b5c8e8cda300c3d10622c2b3490702dcd661
anoopjainhdp <ajain@hortonworks.com>
2015-10-29 14:23:38 -0700

    Added code for try catch

--- BuildNumber.py
+++ BuildNumber.py
@@ -21,48 +21,53 @@ toEmail = sys.argv[3]
 msgText = ''
 msgSubject = 'Build issues with '+buildNum + ' for '
 alert = False
+errMessage = ''
 
 # Repeat the following steps for all the OS combinations
 for operatingSystem,build in OSBuildMap.items():
     # Generate the QE and Dev Build Repo Url
-   
+
     extn = '.repo'
-    
+
     if(operatingSystem.startswith('UBUNTU') or operatingSystem.startswith('DEBIAN') ):
-    	extn = '.list'
-    	
+        extn = '.list'
+
     qeUrl = build+'/updates/'+buildNum+'/ambariqe'+extn
     bnUrl = build+'/latest/'+buildNum+'/ambaribn'+extn
     
     print qeUrl
     print bnUrl
- 
-    req = urllib2.Request(qeUrl)
-    res = urllib2.urlopen(req)
-    response = res.read()
-    qeVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
-    
-    req = urllib2.Request(bnUrl)
-    res = urllib2.urlopen(req)
-    response = res.read()
-    bnVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
 
-    # Get the build difference between the Dev and QE Repos
-    diff = int(bnVersionNum.group(1)) - int(qeVersionNum.group(1))    
-    
-    print "Operating System " , operatingSystem
-    print "QE Build Number ",qeVersionNum.group(1)
-    print "Compiled Build Number ",bnVersionNum.group(1)
+    try:
+        req = urllib2.Request(qeUrl)
+        res = urllib2.urlopen(req)
+        response = res.read()
+        qeVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
 
-    if(diff > int(diff4Alert)) :
-		print '==============Alert===================='
-		msgText += '=====================================\n'
-		msgText += 'Operating System :'+operatingSystem+'\nQE Build Number :'+qeVersionNum.group(1)+'\nCompiled Build Number :'+bnVersionNum.group(1) 
-		msgText +='\n=====================================\n'
-		if(alert):
-		    msgSubject += ' , '
-		msgSubject += operatingSystem
-		alert = True
-	
+        req = urllib2.Request(bnUrl)
+        res = urllib2.urlopen(req)
+        response = res.read()
+        bnVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
+
+        # Get the build difference between the Dev and QE Repos
+        diff = int(bnVersionNum.group(1)) - int(qeVersionNum.group(1))
+
+        print "Operating System " , operatingSystem
+        print "QE Build Number ",qeVersionNum.group(1)
+        print "Compiled Build Number ",bnVersionNum.group(1)
+
+        if(diff > int(diff4Alert)) :
+            print '==============Alert===================='
+            msgText += '=====================================\n'
+            msgText += 'Operating System :'+operatingSystem+'\nQE Build Number :'+qeVersionNum.group(1)+'\nCompiled Build Number :'+bnVersionNum.group(1)
+            msgText +='\n=====================================\n'
+            if(alert):
+                msgSubject += ' , '
+                msgSubject += operatingSystem
+                alert = True
+    except:
+        print "Exception for Operating System " , operatingSystem
+        print sys.exc_info()[0]
+
 if(alert):
-	os.system('echo "'+msgText+'" | mail -s "'+msgSubject+'" '+toEmail)	
+        os.system('echo "'+msgText+'" | mail -s "'+msgSubject+'" '+toEmail)
