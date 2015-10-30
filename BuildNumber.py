3355d078009158b886949cada899e4109b0b07e1
anoopjainhdp <ajain@hortonworks.com>
2015-10-30 11:30:13 -0700

    Added logic for multiple build

--- BuildNumber.py
+++ BuildNumber.py
@@ -15,59 +15,109 @@ OSBuildMap['SUSE11']="http://dev.hortonworks.com.s3.amazonaws.com/ambari/suse11/
 OSBuildMap['DEBIAN7']="http://dev.hortonworks.com.s3.amazonaws.com/ambari/debian7/2.x"
 
 
-buildNum = sys.argv[1]
+buildNumString = sys.argv[1]
 diff4Alert = sys.argv[2]
 toEmail = sys.argv[3]
-msgText = ''
-msgSubject = 'Build issues with '+buildNum + ' for '
-alert = False
-errMessage = ''
 
-# Repeat the following steps for all the OS combinations
-for operatingSystem,build in OSBuildMap.items():
-    # Generate the QE and Dev Build Repo Url
+finalAlert = False
+alert ={}
+msgText ={}
+msgSubject={}
+buildNumArray = buildNumString.split(',')
 
-    extn = '.repo'
+msgFinalText= ''
+msgFinalSubject = ''
 
-    if(operatingSystem.startswith('UBUNTU') or operatingSystem.startswith('DEBIAN') ):
-        extn = '.list'
+releaseOSBuildMap = {}
 
-    qeUrl = build+'/updates/'+buildNum+'/ambariqe'+extn
-    bnUrl = build+'/latest/'+buildNum+'/ambaribn'+extn
+for buildNum in buildNumArray:
+    alert[buildNum] = False
+    msgSubject[buildNum] = 'Build Issue With Release '+buildNum+'('
+    releaseOSBuildMap[buildNum] = {}
     
-    print qeUrl
-    print bnUrl
+    print '\n-----------Release Number : '+buildNum+'--------------\n'
+    # Repeat the following steps for all the OS combinations
+    for operatingSystem,buildUrl in OSBuildMap.items():
+        # Generate the QE and Dev Build Repo Url
 
-    try:
-        req = urllib2.Request(qeUrl)
-        res = urllib2.urlopen(req)
-        response = res.read()
-        qeVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
+        extn = '.repo'
 
-        req = urllib2.Request(bnUrl)
-        res = urllib2.urlopen(req)
-        response = res.read()
-        bnVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
+        if(operatingSystem.startswith('UBUNTU') or operatingSystem.startswith('DEBIAN') ):
+            extn = '.list'
 
-        # Get the build difference between the Dev and QE Repos
-        diff = int(bnVersionNum.group(1)) - int(qeVersionNum.group(1))
+        qeUrl = buildUrl+'/updates/'+buildNum+'/ambariqe'+extn
+        bnUrl = buildUrl+'/latest/'+buildNum+'/ambaribn'+extn
+    
+        print qeUrl
+        print bnUrl
 
-        print "Operating System " , operatingSystem
-        print "QE Build Number ",qeVersionNum.group(1)
-        print "Compiled Build Number ",bnVersionNum.group(1)
+        try:
+            req = urllib2.Request(qeUrl)
+            res = urllib2.urlopen(req)
+            response = res.read()
+            qeVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
 
-        if(diff > int(diff4Alert)) :
-            print '==============Alert===================='
-            msgText += '=====================================\n'
-            msgText += 'Operating System :'+operatingSystem+'\nQE Build Number :'+qeVersionNum.group(1)+'\nCompiled Build Number :'+bnVersionNum.group(1)
-            msgText +='\n=====================================\n'
-            if(alert):
-                msgSubject += ' , '
-                msgSubject += operatingSystem
-            alert = True
-    except:
-        print "Exception for Operating System " , operatingSystem
-        print sys.exc_info()[0]
+            req = urllib2.Request(bnUrl)
+            res = urllib2.urlopen(req)
+            response = res.read()
+            bnVersionNum = re.search('VERSION_NUMBER=.*-([0-9]*)',response)
 
-if(alert):
-        os.system('echo "'+msgText+'" | mail -s "'+msgSubject+'" '+toEmail)
+            # Get the build difference between the Dev and QE Repos
+            diff = int(bnVersionNum.group(1)) - int(qeVersionNum.group(1))
+
+            print "Operating System " , operatingSystem
+            print "QE Build Number ",qeVersionNum.group(1)
+            print "Compiled Build Number ",bnVersionNum.group(1)
+            
+            releaseOSBuildMap[buildNum][operatingSystem] = qeVersionNum.group(1)
+
+            if(diff > int(diff4Alert)) :
+                print '==============Alert Generated For Release '+buildNum+'===================='
+                if(not alert[buildNum]):
+                    msgText[buildNum] = '\n-------------------Release '+buildNum+'------------------\n'
+                    
+                    
+                msgText[buildNum] += '=====================================\n'
+                msgText[buildNum] += 'Operating System :'+operatingSystem+'\nQE Build Number :'+qeVersionNum.group(1)+'\nCompiled Build Number :'+bnVersionNum.group(1)
+                msgText[buildNum] +='\n=====================================\n'
+                
+                
+                if(alert[buildNum]):
+                    msgSubject[buildNum] += ' , '
+                    
+                msgSubject[buildNum] += operatingSystem
+                
+                alert[buildNum] = True
+                finalAlert = True
+        except:
+            print "Exception for Operating System " , operatingSystem
+            print sys.exc_info()[0]
+            
+    msgSubject[buildNum] +=') '
+
+
+for buildNum,subject in msgSubject.items():
+    msgFinalSubject += subject
+
+for buildNum,text in msgText.items():
+    msgFinalText += text
+
+msgFooterText = ''
+
+for buildNum,osBuildMap in releaseOSBuildMap.items():
+    maxBuildNum = 0
+    maxBuildOS = ''
+    for operatingSystem,qeBuild in osBuildMap.items():
+        if(int(qeBuild) > maxBuildNum):
+            maxBuildNum = int(qeBuild)
+            maxBuildOS = operatingSystem
+        elif(int(qeBuild) == maxBuildNum):
+            maxBuildOS += ','+operatingSystem
+    
+    msgFooterText += '\nSuggested Build For Release '+buildNum +' : '+ str(maxBuildNum)  +' on '+maxBuildOS
+    
+msgFinalText += '\n\n'+msgFooterText
+    
+if(finalAlert):
+        os.system('echo "'+msgFinalText+'" | mail -s "'+msgFinalSubject+'" '+toEmail)
+        print 'Mail Sent Successfully to '+toEmail
